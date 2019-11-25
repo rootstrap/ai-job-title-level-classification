0bdeab14f29f0311c075b13f12369dff54d89783
Bruno Michetti <brunomichettileites@gmail.com>
2019-11-25 18:39:57 -0300

    First commit.

---
+++ data_process/data_sets/values_and_labels_dicts.py
@@ -0,0 +1,15 @@
+area_value_label_dict = {
+    0: 'C-Suite',
+    1: 'VP',
+    2: 'Director',
+    3: 'Manager',
+    4: 'Other'
+}
+
+area_label_value_dict = {
+    'C-Suite': 0,
+    'VP': 1,
+    'Director': 2,
+    'Manager': 3,
+    'Other': 4
+}
